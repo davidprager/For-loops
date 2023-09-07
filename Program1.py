d0b9ecc92d21b881af0a7740db7001ca7bf38ea1
davidprager <david_prager@yahoo.com>
2023-09-07 18:44:39 -0400

    Initial commit

---
+++ Program1.py
@@ -0,0 +1,7 @@
+"""
+CSCI-UA.0002 Spring 2024
+Assignment #5, Program 1
+Your name here
+"""
+
+# This program determines the least profitable floor in a building.
---
+++ Program2.py
@@ -0,0 +1,7 @@
+"""
+CSCI-UA.0002 Spring 2024
+Assignment #5, Program 2
+Your name here
+"""
+
+# This program finds Prime Numbers based upon user input
